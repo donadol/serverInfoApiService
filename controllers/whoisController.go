bda657857679a003efbd931eda77e493c5a6687b
Laura Donado <ldonado11@gmail.com>
2019-12-01 11:28:15 -0500

    Create whoisController.go

---
+++ controllers/whoisController.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+
+	"../models"
+)
+
+func Whois(domain string, w http.ResponseWriter) models.Domain {
+	response, err := http.Get("http://ip-api.com/json/" + domain)
+	if err != nil {
+        fmt.Printf("The HTTP request failed with error %s\n", err)
+    } else {
+        data, _ := ioutil.ReadAll(response.Body)
+        //fmt.Println(string(data))
+		ip := models.Domain{}
+		err = json.Unmarshal(data, &ip)
+		//fmt.Printf("%+v", ip)
+		return ip
+	}
+	return models.Domain{}
+}
