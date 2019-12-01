f033875bfdb95eeb7cb70cc1fad2b9c04ea023d7
Laura Donado <ldonado11@gmail.com>
2019-12-01 12:07:33 -0500

    Update sslController.go

--- controllers/sslController.go
+++ controllers/sslController.go
@@ -9,17 +9,17 @@ import (
 	"../models"
 )
 
-func ssl(domain string, w http.ResponseWriter) models.Host {
+func Ssl(domain string, w http.ResponseWriter) models.Host {
 	response, err := http.Get("https://api.ssllabs.com/api/v3/analyze?host=" + domain)
 	if err != nil {
         fmt.Printf("The HTTP request failed with error %s\n", err)
     } else {
         data, _ := ioutil.ReadAll(response.Body)
         //fmt.Println(string(data))
-		ip := models.Host{}
-		err = json.Unmarshal(data, &ip)
+		host := models.Host{}
+		err = json.Unmarshal(data, &host)
 		//fmt.Printf("%+v", ip)
-		return ip
+		return host
 	}
 	return models.Host{}
 }
