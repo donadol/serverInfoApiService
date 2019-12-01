d4b415d6d11ad19a39790173ab6ea1d49fc50c06
Laura Donado <ldonado11@gmail.com>
2019-12-01 10:51:38 -0500

    added script

---
+++ script.sh
@@ -0,0 +1,12 @@
+go get -u github.com/lib/pq
+
+go get -u github.com/go-chi/chi
+
+cockroach start-single-node \
+--insecure \
+--listen-addr=localhost:26257 \
+--http-addr=localhost:8080 \
+--background
+
+cockroach init --insecure --host=localhost:26257
+cockroach sql --insecure < statements.sql
