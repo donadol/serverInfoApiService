374d42ebdd27f8c7cbf003398ec4f35f683ea376
Laura Donado <ldonado11@gmail.com>
2019-12-01 21:23:55 -0500

    Update main.go

--- main.go
+++ main.go
@@ -13,13 +13,11 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.URLFormat)
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Get("/servers", controllers.Records)
+	//r.Get("/servers", controllers.InfoServers)
 	r.Get("/{domain}", controllers.InfoDomain)
 
 	http.ListenAndServe(":3000", r)
