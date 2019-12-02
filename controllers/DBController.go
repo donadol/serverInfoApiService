5a22a83c1d77ddf3c2e03c47d316c65c41aad279
Laura Donado <ldonado11@gmail.com>
2019-12-01 22:25:09 -0500

    Update DBController.go

--- controllers/DBController.go
+++ controllers/DBController.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"database/sql"
 	"log"
-	"fmt"
 
 	"../models"
 	_ "github.com/lib/pq"
@@ -58,7 +57,7 @@ func InsertServer(info models.Server, id int64) {
 	}
 	stmt, err := db.Prepare("INSERT INTO SERVER (address, ssl_grade, country, owner, infoserver_id) VALUES ($1, $2, $3, $4, $5);")
 	if err != nil {
-		log.Fatal("Error inserting: ", err)
+		log.Fatal("Error inserting s: ", err)
 	}
 	defer stmt.Close()
 	
@@ -128,10 +127,10 @@ func FindServersRecords()(models.ServerRecord){
 	if err != nil {
 		log.Fatal("Error connecting to the DB: ", err)
 	}
-	stmt, err := db.Prepare(`SELECT DISTINCT id, host, MAX(consulted_time)
+	stmt, err := db.Prepare(`SELECT DISTINCT ON (host) host, id
 							FROM DOMAIN
-							GROUP BY host
-							ORDER BY consulted_time DESC, host;`)
+							GROUP BY host, id
+							ORDER BY host, MAX(consulted_time), id;`)
 	if err != nil {
 		log.Fatal("Error in select: ", err)
 	}
@@ -146,13 +145,79 @@ func FindServersRecords()(models.ServerRecord){
 
 	for rows.Next() {
 		var item models.Item
-        if err = rows.Scan(&item.Domain); err != nil {
+		var id int64
+		err = rows.Scan(&item.Domain, &id)
+        if err != nil {
             log.Fatal(err)
 		}
-		
+		item.InfoServers = FindHosts(id)
         items = append(items, item)
 	}
 	record.Items = items
 	db.Close()
 	return record
+}
+
+func FindHosts(id int64)([]models.InfoServer){
+	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/serversinfo?sslmode=disable")
+	if err != nil {
+		log.Fatal("Error connecting to the DB: ", err)
+	}
+	stmt, err := db.Prepare(`SELECT id, servers_changed, ssl_grade, previous_ssl_grade, logo, title, is_down
+							FROM INFOSERVER
+							WHERE domain_id = $1;`)
+	if err != nil {
+		log.Fatal("Error in select: ", err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(id)
+	if err != nil {
+        log.Fatal(err)
+	}
+	defer rows.Close()
+	hosts := []models.InfoServer{}
+
+	for rows.Next() {
+		var host models.InfoServer
+		var host_id int64
+		err = rows.Scan(&host_id, &host.ServersChanged, &host.MinGrade, &host.PreviousMinGrade, &host.Logo, &host.Title, &host.IsDown)
+        if err != nil {
+            log.Fatal(err)
+		}
+		host.Servers = FindServers(host_id)
+        hosts = append(hosts, host)
+	}
+	db.Close()
+	return hosts
+}
+
+func FindServers(id int64)([]models.Server){
+	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/serversinfo?sslmode=disable")
+	if err != nil {
+		log.Fatal("Error connecting to the DB: ", err)
+	}
+	stmt, err := db.Prepare(`SELECT address, ssl_grade, country, owner
+							FROM SERVER
+							WHERE infoserver_id = $1;`)
+	if err != nil {
+		log.Fatal("Error in select: ", err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(id)
+	if err != nil {
+        log.Fatal(err)
+	}
+	defer rows.Close()
+	servers := []models.Server{}
+
+	for rows.Next() {
+		var server models.Server
+		err = rows.Scan(&server.Address, &server.Grade, &server.Country, &server.Owner)
+        if err != nil {
+            log.Fatal(err)
+		}
+        servers = append(servers, server)
+	}
+	db.Close()
+	return servers
 }
